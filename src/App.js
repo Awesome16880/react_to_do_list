af6b0a38cf1f0138e30b1b9dbfddc5b467c43624
Awesome16880 <awesome16880@gmail.com>
2021-07-27 22:46:28 -0400

    iniital

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+import { useState } from "react";
+import List from "./components/List";
+
+const {default: Form} = require("./components/Form");
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleInput = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
+  };
+
+  const handleSubmit = () => {
+    const newTodo = {
+      title: inputValue,
+      isDone: false,
+    };
+    const nextTodos = [...todos];
+    nextTodos.push(newTodo);
+    setTodos(nextTodos);
+    setInputValue("");
+  };
+
+  const handleDelete = (i) => {
+    const nextTodos = [...todos];
+    nextTodos.splice(i, 1);
+    setTodos(nextTodos);
+  };
+
+  const handleDone = (i) => {
+    const nextTodos = [...todos];
+    nextTodos[i].isDone = true;
+    setTodos(nextTodos);
+  };
+
+  const handleUndone = (i) => {
+    const nextTodos = [...todos];
+    nextTodos[i].isDone = false;
+    setTodos(nextTodos);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List
+        todos={todos}
+        handleDelete={handleDelete}
+        handleUndone={handleUndone}
+        handleDone={handleDone}
+      />
+      <Form
+        inputValue={inputValue}
+        handleInput={handleInput}
+        handleSubmit={handleSubmit}
+      />
     </div>
-  );
+  )
 }
+/*const List = props => (
+  <div>
+    {props.items.map((item, index) => (
+      <div className={className = isDone? Done : NotDone}>
+        <li key={index}>{item}</li>
+        <button onClick={items.splice(index, 1)}> X </button>
+        ))}
+      </div>
+  </div>
+);
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      items: [],
+      isDone: false,
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({term: event.target.value});
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      term: '',
+      items: [...this.state.items, this.state.term]
+    });
+  }
+
+  render () {
+    return(
+      <div className="App">
+        <List items={this.state.items} />
+        <form className="Form" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+*/
 export default App;
---
+++ src/components/List/index.js
@@ -0,0 +1,22 @@
+
+
+const List = ({ todos, handleDelete, handleDone, handleUndone }) => {
+    return (
+        <div>
+            {todos.map((v, i) => {
+                return (
+                    <div key={i} className="todo">
+                        <input 
+                            onChange={v.isDone ? () => handleUndone(i) : () => handleDone(i)}
+                            type="checkbox"
+                            checked={v.isDone}
+                        />
+                        <span className={v.isDone ? "Done" : "notDone"}>{v.title}</span>
+                        <button onClick={() => handleDelete(i)}>x</button>
+                    </div>
+                );
+            })}
+        </div>
+        );
+    }
+export default List;
